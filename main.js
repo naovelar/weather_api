9d94b120fd8f6ddccc67f55389c021d651db1f1a
nhporti2 <nhporti2@gmail.com>
2021-05-24 12:13:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,42 +1,53 @@
 // API key from OpenWeather
+const apiKey = "Y2b5ff1a3850dba5620b735e5d5710108";
+const inputVal = input.value;
 
-const api = '2b5ff1a3850dba5620b735e5d5710108';
-
-const loc = document.querySelector('#location');
-const tempC = document.querySelector('.c');
-const tempF = document.querySelector('.f');
-const desc = document.querySelector('.desc');
-
-// include event listener to react to an input or signal by calling the event's handler
-window.addEventListener('load', () => {
-  let long;
-  let lat;
-
-  // accesing location of user using a promise
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      // storing longitude and latitude in variables
-      long = position.coords.longitude;
-      lat = position.coords.latitude;
-      const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`;
-
-      // using fetch to get data
-      fetch(base)
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          const { temp } = data.main;
-          const place = data.name;
-          const { description } = data.weather[0];
-          const fahrenheit = (temp * 9) / 5 + 32;
-
-          // interacting with DOM to show data
-          loc.textContent = `${place}`;
-          desc.textContent = `${description}`;
-          tempC.textContent = `${temp.toFixed(2)} °C`;
-          tempF.textContent = `${fahrenheit.toFixed(2)} °F`;
-        });
-    });
-  }
+const form = document.querySelector(".top-banner form");
+ 
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const inputVal = input.value;
 });
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    // do stuff with the data
+  })
+  .catch(() => {
+    msg.textContent = "Please search for a valid city";
+  });
+
+  const listItems = list.querySelectorAll(".ajax-section .city");
+const listItemsArray = Array.from(listItems);
+ 
+if (listItemsArray.length > 0) {
+  const filteredArray = listItemsArray.filter(el => {
+    let content = "";
+
+    if (inputVal.includes(",")) {
+    
+      if (inputVal.split(",")[1].length > 2) {
+        inputVal = inputVal.split(",")[0];
+        content = el.querySelector(".city-name span").textContent.toLowerCase();
+      } else {
+        content = el.querySelector(".city-name").dataset.name.toLowerCase();
+      }
+    } else {
+
+      content = el.querySelector(".city-name span").textContent.toLowerCase();
+    }
+    return content == inputVal.toLowerCase();
+  });
+   
+  if (filteredArray.length > 0) {
+    msg.textContent = `You already know the weather for ${
+      filteredArray[0].querySelector(".city-name span").textContent
+    } ...otherwise be more specific by providing the country code as well`;
+    form.reset();
+    input.focus();
+    return;
+  }
+}
